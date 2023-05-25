0dec1bf87a23f0085fb06ee1c0c9c2318794de1b
meraff123 <113929284+meraff123@users.noreply.github.com>
2023-05-24 20:38:32 -0500

    working on a question components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Question from './Components/Question';
+
+const quizData = [
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Rome"],
+    correctAnswer: "Paris"
+  },
+  {
+    question: "which planet is known as the Red Planet?",
+    options: ["venus", "Mars", "Jupiter", "Saturn"],
+    correctAnswer: "Mars"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Question  quizDatas = {quizData} />
+      
     </div>
   );
 }
---
+++ src/Components/Question.js
@@ -0,0 +1,28 @@
+ 
+ function Question (props){
+    return (
+        <div> 
+        <h1> Quiz app </h1>
+        <ul>
+            {
+            props.quizDatas.map(quiz =>
+                <li>
+                    <h2> {quiz.question} </h2>
+                    <ul>
+                       {quiz.options.map(option =>
+                        <li> {option} </li>)}
+                    </ul>
+                </li> )
+            }
+        </ul>
+        </div>
+    )
+ }
+ export default Question
+ 
+
+  
+
+  
+
+ 
